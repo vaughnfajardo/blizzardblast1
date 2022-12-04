e4a06e93b8e3e4b17baa46383c9529ed0d4b9cd7
cai <colleen.barrameda@obf.ateneo.edu>
2022-12-04 16:54:47 +0800

    Added new urls for the created compile view

--- orderpreparation/urls.py
+++ orderpreparation/urls.py
@@ -5,5 +5,10 @@ app_name = 'orderpreparation'
 urlpatterns = [
     path("orderpreparation/", HomePageView.as_view(), name='index'),
     path("refillingredient/", refillingredient, name = 'refillingredient'),
-    path("orderpreparation/<int:customer_id>/", orderslip, name='orderslip'),
+   #  path("orderpreparation/<int:customer_id>/", orderslip,),
+
+
+    path("orders/", OrdersPageView.as_view(), name='blizzardblast'),
+    path("orders/<int:customer_id>/", orderslip, name='orderslip'),
+    path("orders/add/", OrdersCreateView.as_view(), name='new-order'),
 ]
